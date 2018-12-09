0505eb6b72705e4c3242bbc5f0dae2b5cf2e8f43
Iftifar Taz <iftifar.taz@northsouth.edu>
2018-12-09 17:11:08 +0600

    package cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import 'hammerjs';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {MatCardModule} from '@angular/material/card';
